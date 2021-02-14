350ab7d5e53c25d53c13ebf9857fa0b5f62dec5d
Dmitry Landy <dmitry.landy@outlook.com>
2021-02-13 23:06:45 -0500

    taxi fare files

---
+++ Taxi-Fare/taxiFare.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Name: Dmitry Landy
+ * File: taxiFare.cs
+ * Date: 2/13/2021
+ */
+using System;
+using TaxiFareML.Model;
+
+namespace taxiFare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Add input data
+            var input = new ModelInput();
+            input.Trip_distance = 300;
+            input.Trip_time_in_secs = 1500;
+
+            // Load model and predict output of sample data
+            ModelOutput result = ConsumeModel.Predict(input);
+            Console.WriteLine($"Distance: {input.Trip_distance}, " +
+                $"Time: {input.Trip_time_in_secs}");
+            Console.WriteLine($"Predicted cost: {result.Score}");
+        }
+    }
+}
